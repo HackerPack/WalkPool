d04c8e11803131956563e54c89b8189a2697f08b
Diksha <Diksha@dyn-10-180-17-64.eduroam.duke.local>
2015-11-07 21:13:49 -0500

    Facebook authentication

---
+++ js/authentication.js
@@ -0,0 +1,11 @@
+function facebookLogin()
+{
+	var ref = new Firebase("https://brilliant-fire-4087.firebaseio.com");
+ref.authWithOAuthPopup("facebook", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
+}
