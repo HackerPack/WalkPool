8b5f64a9f389270b235fb15c53b6482f7834290d
Diksha <Diksha@dyn-10-180-17-64.eduroam.duke.local>
2015-11-08 05:33:42 -0500

    Some changes

--- js/dbAPI.js
+++ js/dbAPI.js
@@ -113,7 +113,7 @@ function getAllEvents() {
 		eventList.forEach(function(eventSnap) {
 			var eventID = eventSnap.key();
 			ref.child("Users").orderByKey().equalTo(eventSnap.val().UID).once("value",function(userDataSnap){
-				 var userFName = userDataSnap.key();
+				 var userFName = userDataSnap.val().FirstName;
 		console.log("FNAME"+userFName);
 		console.log(eventID);
 
@@ -127,7 +127,7 @@ function getAllEvents() {
 				});
 		});
 	});
-	alert(allEvents[0]);
+	console.log(allEvents[0]);
 	return allEvents;
 }
 
